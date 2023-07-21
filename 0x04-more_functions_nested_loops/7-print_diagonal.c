7f433d58e65037aef27956e9f808e01cb49a9df4
Listone <koechlistone@gmail.com>
2023-07-20 17:53:25 -0700

    I feel like I am diagonally parked in a parallel universe

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,23 +1,25 @@
-#include 'main.h"
+#include "main.h"
 
 /**
- * print_diagonal - printing lines in diagonal
- * @n : input variable
- *
+ * print_diagonal - printing lines diagonal
+ * @n: input variable
  * Return: void
- * */
+ */
 
 void print_diagonal(int n)
-int i, j;
+{
+	int i, j;
 
-if (n <= 0)
-_putchar('\n');
-for (i= 0; i < n; i++)
-{
-for (j = 0; j < i; j++)
-{
-_putchar(' ');
+	if (n <= 0)
+	_putchar('\n');
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
 }
-_putchar('\\');
-_Putchar('\n');
-}
+
